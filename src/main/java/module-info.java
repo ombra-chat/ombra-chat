6a4649bb5357d415508686812b9b82f6fe41b458
Sonia Zorba <sonia.zorba@mailbox.org>
2025-01-26 01:44:41 +0100

    Added/updated dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module net.zonia3000 {
     requires javafx.controls;
-    exports net.zonia3000;
+    requires javafx.fxml;
+    requires tdlib;
+    requires java.prefs;
+    exports net.zonia3000.ombrachat;
+    exports net.zonia3000.ombrachat.login;
+    opens net.zonia3000.ombrachat.login to javafx.fxml;
 }
