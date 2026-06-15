386d50c0812217c6e0eaa716bf3d96b58c1a81dc
Sonia Zorba <sonia.zorba@mailbox.org>
2026-06-16 00:27:08 +0200

    Removed unused import

--- src/Main.vue
+++ src/Main.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted } from 'vue';
 import ChatFolders from './ChatFolders.vue';
 import Sidebar from './Sidebar.vue';
 import { store } from './store';
